c1a72f44c2f982ea3fd49fd163b31dfd6251b7d9
sethcarrino <sethcarrino@gmail.com>
2017-08-28 08:46:12 -0400

    functionality of the app now works - still need to add styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,88 +2,104 @@ import React, {Component} from 'react';
 import '../styles/App.css';
 
 class App extends Component {
-  // PROPS AND STATE
-  // Set props and state below.
-  // You should set state for vehicles (empty array), value (empty string), pilot (empty) string.
-  // Enter your code below:
-  constructor(props){
+  constructor(props) {
     super(props);
-
+    // bind event handlers
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //set state inside of constructor
     this.state = {
       vehicles: [],
-      value: '',
+      name: '',
       pilot: ''
     };
+  }
 
-    this.handleNamechange = this.handleNameChange.bind(this);
-    // this.hamdleSubmit = this.handleSubmit.bind(this);
+  //handle input changes
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+  }
 
+  // sets what happens after form is submitted
+  handleSubmit(event) {
+    // prevent early submission
+    event.preventDefault();
+    // use state
+    this.setState(
+      {
+        pilot: this.state.value,
+        name: ''
+      }
+    )
   }
 
 
-
-
-  // FORM: HANDLE INPUT CHANGES
-  // handleNameChange below:
-  // See form lesson for details.
-  // Enter your code below:
-
-  handleNameChange(event){
-    this.setState({
-      name: event.target.value
-    })
-  }
-
-  //  FORM: SUBMIT METHOD
-  // handleSubmit below:
-  // See form lesson for details.
-  // Once the form is sumbited, two things need to happen: set the state of pilot to the input value.
-  // Then, set the value of the input back to an empty string.
-  // Enter your code below:
-
-
-  // LIFECYCLE
-  // Which lifecycle is best for fetching data?
-  // Inside this lifecycle, you will fetch the vehicles from here: https://swapi.co/api/vehicles/
-  // Once you have fetched that data, set the state of vehicles to the fetched data.
-  // In your response look for 'results'. It should return this array.
-  // You will want to use this array when you set the state of 'vehicles'. You will need this data in your render.
-  // Enter your code below:
-
-  componentWillMount() {
+  // fetch data and set state of vehicles
+  componentDidMount() {
     fetch('https://swapi.co/api/vehicles/')
-    .then(r => r.json() )
-    .then((json) => {
-      console.log("Data from componentWillMount fetch", json)
-      this.setState({nasa: json})
+    .then((response) => {
+      return response.json()
+  })
+    .then((data) => {
+      let vehicles = data.results;
+      console.log(vehicles);
+      this.setState({vehicles:vehicles})
     })
   }
-
-  // RENDER
-  // Before you can map over the data you've fetched, you will first need to store that 'state' in a variable.
-  // Map over the data.
-  // Don't forget to set the 'key'. In this case, use the vehicle name.
-  // You will need the following values: name, model, manufacturer, class, passengers, crew, length, max speed, and cargo capacity.
-  // Rendering: create a 'card' for each of the vehicles. consult the Bootstrap 4 docs for details.
-  // Enter your code below:
 
   render() {
-    /*
-    Store vehicles state in a variable.
-    Map over this variable to access the values needed to render.
-    */
-
+    let vehicleList = this.state.vehicles;
+    let vehicles = vehicleList.map((vehicles) => {
+      return (
+        <div key={vehicles.name}>
+          <div>
+            <h3>Vehicle: {vehicles.name}</h3>
+            <h4>Model: {vehicles.model}</h4>
+            <div>
+              <div>
+                <h4>Specs</h4>
+                <ul>
+                  <li>Manufacturer: {vehicles.manufacturer}</li>
+                  <li>Class: {vehicles.vehicle_class}</li>
+                  <li>passengers: {vehicles.passengers}</li>
+                  <li>Crew: {vehicles.crew}</li>
+                  <li>Length: {vehicles.length}</li>
+                  <li>Max Speed: {vehicles.max_atsmospering_speed}</li>
+                  <li>Cargo Capacity: {vehicles.cargo_capacity}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    })
 
     return (
       <div className="App">
-        {/*
-        The App component needs the following:
-         jumbotron section, form section, vehicle cards section.
-         Your form will also need a header in which you will pass the state of the form upon submit.
-         */
-       }
+      <div>
+        <div>
+          <div>
+            <h1>Star Wars</h1> <hr/> <p> The Vehicles of Star Wars </p>
+          </div>
+          <div>
+            <div>
+              <h2>What is your name, pilot?</h2>
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <input id="name" onChange={this.handleNameChange} name="name" type="text" value={this.state.name} placeholder="Enter your name"/>
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+              <h1>{this.state.pilot}</h1>
+            </div>
+          </div>
+          <div>
+            {vehicles}
+          </div>
+        </div>
       </div>
-    );
+      </div>
+    )
   }
 }
 
